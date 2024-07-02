57608c0f8bb19f6f8c5eb786ae73342429aa4026
Nicole Venner <nvenner@protonmail.ch>
2024-07-02 07:47:40 -0600

    image output actually working

--- main.zig
+++ main.zig
@@ -11,12 +11,12 @@ pub fn main() void {
     std.debug.print("P3\n{} {}\n255\n", .{ image_width, image_height });
     for (0..image_height) |j| {
         for (0..image_width) |i| {
-            const r = @as(f64, i) / (image_width - 1);
-            const g = @as(f64, j) / (image_height - 1);
+            const r: f64 = @as(f64, @floatFromInt(i)) / (image_width - 1);
+            const g: f64 = @as(f64, @floatFromInt(j)) / (image_height - 1);
             const b: f64 = 0.0;
-            const ir = @as(i32, r * 255.999);
-            const ig = @as(i32, g * 255.999);
-            const ib = @as(i32, b * 255.999);
+            const ir = @as(i32, @intFromFloat(r * 255.999));
+            const ig = @as(i32, @intFromFloat(g * 255.999));
+            const ib = @as(i32, @intFromFloat(b * 255.999));
             std.debug.print("{} {} {}\n", .{ ir, ig, ib });
         }
     }
