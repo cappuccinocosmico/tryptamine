6c492391a92b4e6a63f4300f5f222a200b776c97
Nicole Venner <nvenner@protonmail.ch>
2024-10-08 12:07:20 -0600

    inheritence annoying

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use axum::{
 };
 use tower_http::services::ServeDir;
 #[derive(Template)] // this will generate the code...
-#[template(path = "test.html")] // using the template in this path, relative
-                                // to the `templates` dir in the crate root
+#[template(path = "app.html")] // using the template in this path, relative
+                               // to the `templates` dir in the crate root
 struct HelloTemplate<'a> {
     // the name of the struct can be anything
     name: &'a str, // the field name should match the variable name
