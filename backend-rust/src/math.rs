970100b92b989c2f6831dccb967bb59b9af17bca
Nicole Venner <nvenner@protonmail.ch>
2024-12-20 08:05:48 -0700

    even more number sieve stuff

--- backend-rust/src/math.rs
+++ backend-rust/src/math.rs
@@ -1,14 +1,24 @@
 use num::{FromPrimitive, One};
 use num_bigint::BigUint;
 use num_complex::Complex;
+
+const SMALL_PRIMES: [usize; 58] = [
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+    101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193,
+    197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271,
+];
+
 fn miller_rabin_primality(num: &BigUint) -> bool {
+    if num == &BigUint::ZERO {
+        return false;
+    }
     fn even_exp_factorize(num: &BigUint) -> (BigUint, u64) {
         let exp = num.trailing_zeros();
         if let Some(exp) = exp {
             let odd = num << exp;
             return (odd, exp);
         }
-        panic!("num is zero");
+        panic!("Somehow you encountered unreachable code by passing zero into the primality function, dispite a check for zero existing.")
     }
 
     fn miller_rabin_iteration(
@@ -51,17 +61,69 @@ fn eitau_real(x: &f32) -> Complex<f32> {
 }
 
 fn small_prime_factorize(num: &BigUint) -> Option<[BigUint; 2]> {
-    let small_primes: Vec<u32> = vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47];
     // let largest_small_prime = small_primes[small_primes.len() - 1];
-    for prime in small_primes {
+    for prime in SMALL_PRIMES {
         if num % prime == BigUint::ZERO {
-            return Some([num.clone() / prime, BigUint::from_u32(prime).unwrap()]);
+            return Some([num.clone() / prime, BigUint::from_usize(prime).unwrap()]);
         }
     }
     None
 }
 
+fn generate_smooth_vector(num: &BigUint, list: &Vec<usize>) -> Option<Vec<u8>> {
+    let mut remainder = num.clone();
+    let mut result_exponents: Vec<u8> = vec![0; list.len()];
+    fn trial_division_singleton_index(num: &BigUint, list: &Vec<usize>) -> Option<usize> {
+        // Chance this for an index.
+        for (index, trial) in list.iter().enumerate() {
+            if num % trial == BigUint::ZERO {
+                return Some(index);
+            }
+        }
+        None
+    }
+    while remainder > BigUint::one() {
+        match trial_division_singleton_index(&remainder, list) {
+            None => return None,
+            Some(index) => {
+                result_exponents[index] += 1;
+                remainder = remainder / list[index];
+            }
+        }
+    }
+    return Some(result_exponents);
+}
+
 fn quadratic_number_sieve_factor(num: &BigUint) -> Option<[BigUint; 2]> {
+    const SMOOTHNESS_DIMENSION: usize = 32;
+    let primes: Vec<usize> = SMALL_PRIMES.to_vec();
+    let mut results: Vec<(BigUint, Vec<u8>)> = Vec::new();
+    let mut test = num.sqrt() + 1 as usize;
+    let mut results_found: usize = 0;
+    while results_found < SMOOTHNESS_DIMENSION + 1 {
+        let test_squared = test.pow(2) % num;
+        if let Some(divisors) = generate_smooth_vector(&test_squared, &primes) {
+            results.push((test.clone(), divisors));
+            results_found += 1;
+        }
+        test += 1 as usize;
+    }
+
+    // So the problem has now been reduced to: given a set of integers, find a subset whose product is a square. By the [[fundamental theorem of arithmetic]], any positive integer can be written uniquely as a product of [[prime power]]s. We do this in a vector format; for example, the prime-power factorization of 504 is 2<sup>3</sup>3<sup>2</sup>5<sup>0</sup>7<sup>1</sup>, it is therefore represented by the exponent vector (3,2,0,1). Multiplying two integers then corresponds to adding their exponent vectors. A number is a square when its exponent vector is even in every coordinate. For example, the vectors (3,2,0,1) + (1,0,0,1) = (4,2,0,2), so (504)(14) is a square. Searching for a square requires knowledge only of the [[parity (mathematics)|parity]] of the numbers in the vectors, so it is sufficient to compute these vectors mod 2: (1,0,0,1) + (1,0,0,1) = (0,0,0,0).
+    // So given a set of (0,1)-vectors, we need to find a subset which adds to the [[zero vector]] mod 2.
+    //
+    // This is a [[linear algebra]] problem since the [[ring (mathematics)|ring]] <math>\mathbb{Z}/2\mathbb{Z}</math> can be regarded as the [[Galois field]] of order 2, that is we can divide by all non-zero numbers (there is only one, namely 1) when calculating modulo 2.
+    // It is a [[Rank–nullity theorem|theorem of linear algebra]] that with more vectors than each vector has entries, a [[linear dependency]] always exists. It can be found by [[Gaussian elimination]].
+    // However, simply squaring many random numbers mod ''n'' produces a very large number of different [[prime number|prime]] factors, and thus very long vectors and a very large matrix. The trick is to look specifically  for numbers ''a'' such that ''a''<sup>2</sup> mod ''n'' has only small prime factors (they are [[smooth number]]s). They are harder to find, but using only smooth numbers keeps the vectors and matrices smaller and more tractable. The quadratic sieve searches for smooth numbers using a technique called [[sieve theory|sieving]], discussed later, from which the algorithm takes its name.
+    //
+    // To summarize, the basic quadratic sieve algorithm has these main steps:
+    //
+    //     Choose a smoothness bound B. The number π(B), denoting the number of prime numbers less than B, will control both the length of the vectors and the number of vectors needed.
+    //     Use sieving to locate π(B) + 1 numbers a_i such that b_i = (a_i^2 mod n) is B-smooth.
+    //     Factor the b_i and generate exponent vectors mod 2 for each one.
+    //     Use linear algebra to find a subset of these vectors which add to the zero vector. Multiply the corresponding ai together and give the result mod n the name a; similarly, multiply the bi together which yields a B-smooth square b2.
+    //     We are now left with the equality a^2 = b^2 mod n from which we get two square roots of (a^2 mod n), one by taking the square root in the integers of b^2 namely b, and the other the a computed in step 4.
+    //     We now have the desired identity: ( a + b ) ( a − b ) ≡ 0 ( mod n ) {\displaystyle (a+b)(a-b)\equiv 0{\pmod {n}}}. Compute the GCD of n with the difference (or sum) of a and b. This produces a factor, although it may be a trivial factor (n or 1). If the factor is trivial, try again with a different linear dependency or different a.
     None
 }
 
