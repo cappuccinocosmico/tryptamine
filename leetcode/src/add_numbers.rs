f1fa7a64a1a8fcfca8de8a094409f04d092af8a5
Nicole Venner <nvenner@protonmail.ch>
2025-07-27 16:06:23 -0600

    implement add two numbers code

--- leetcode/src/add_numbers.rs
+++ leetcode/src/add_numbers.rs
@@ -15,11 +15,31 @@ impl ListNode {
 
 // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 //
+const BASE: i32 = 10;
 struct Solution {}
 impl Solution {
     pub fn add_two_numbers(
         l1: Option<Box<ListNode>>,
         l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
+        let mut borrow = 0;
+        let mut return_node = None;
+        let mut tail = &mut return_node;
+        let (mut l1_ref, mut l2_ref) = (l1.as_ref(), l2.as_ref());
+        while l1_ref.is_some() || l2_ref.is_some() || borrow != 0 {
+            let l1_val = l1_ref.map_or(0, |val| val.val);
+            let l2_val = l2_ref.map_or(0, |val| val.val);
+            let value = borrow + l1_val + l2_val;
+            let (rem, divisor) = (value % BASE, value / BASE);
+            let new_node = ListNode::new(rem);
+            *tail = Some(Box::new(new_node));
+            tail = &mut tail.as_mut().unwrap().next;
+            borrow = divisor;
+
+            l1_ref = l1_ref.map_or(None, |val| (val.next).as_ref());
+            l2_ref = l2_ref.map_or(None, |val| (val.next).as_ref());
+        }
+
+        return_node
     }
 }
