b3c5979f75f01b84a1d73391646c44bf38b652a0
Nicole Venner <nvenner@protonmail.ch>
2025-07-27 14:41:28 -0600

    temporary reverse k nodes

--- leetcode/src/reverse_nodes_k_group.rs
+++ leetcode/src/reverse_nodes_k_group.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 // Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
 //
 // k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
@@ -20,5 +22,31 @@ impl ListNode {
 
 struct Solution {}
 impl Solution {
-    pub fn reverse_k_group(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {}
+    pub fn reverse_k_group(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+        let Some(mut head) = head else { return None };
+        let previous_head = &mut head;
+        loop {
+            let mut rev_head = previous_head.next.take();
+            let mut tail = match &mut rev_head {
+                Some(val) => val,
+                None => {
+                    previous_head.next = rev_head;
+                    return Some(head);
+                }
+            };
+            for _ in 0..k {
+                let Some(new_tail) = &mut tail.next else {
+                    previous_head.next = rev_head;
+                    return Some(head);
+                };
+                tail = new_tail;
+            }
+            let mut next_head = tail.next.take();
+            while let Some(mut new_head) = rev_head.take() {
+                rev_head = new_head.next.take();
+                // let new_head_next = new_head.next.take();
+                new_head.next = next_head;
+            }
+        }
+    }
 }
