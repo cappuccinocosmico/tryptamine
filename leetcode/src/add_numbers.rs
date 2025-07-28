301df8edab19cacb38668fee1c2218f819f66d1e
Nicole Venner <nvenner@protonmail.ch>
2025-07-27 18:52:23 -0600

    aah stuff

--- leetcode/src/add_numbers.rs
+++ leetcode/src/add_numbers.rs
@@ -11,12 +11,12 @@ impl ListNode {
         ListNode { next: None, val }
     }
 }
+struct Solution {}
 //You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
 // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 //
 const BASE: i32 = 10;
-struct Solution {}
 impl Solution {
     pub fn add_two_numbers(
         l1: Option<Box<ListNode>>,
--- leetcode/src/reverse_nodes_k_group.rs
+++ leetcode/src/reverse_nodes_k_group.rs
@@ -1,5 +1,3 @@
-use std::collections::VecDeque;
-
 // Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
 //
 // k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
@@ -21,32 +19,46 @@ impl ListNode {
 }
 
 struct Solution {}
+
+fn pop_and_advance_linked_node(node: &mut Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut owned_node = node.take()?;
+    let next_node = owned_node.next.take();
+    *node = next_node;
+    Some(owned_node)
+}
+
+fn advance_k_in_linked_list(
+    begin_node: &mut Option<Box<ListNode>>,
+    k: i32,
+) -> Option<&mut Box<ListNode>> {
+    let mut begin_ref = begin_node;
+    for _ in 0..k {
+        begin_ref = &mut begin_ref.as_mut()?.next
+    }
+    begin_ref.as_mut()
+}
+
 impl Solution {
     pub fn reverse_k_group(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
-        let Some(mut head) = head else { return None };
-        let previous_head = &mut head;
+        let mut head = head?;
+        let mut previous_head = &mut head;
         loop {
             let mut rev_head = previous_head.next.take();
-            let mut tail = match &mut rev_head {
+            let mut tail = match advance_k_in_linked_list(&mut rev_head, k) {
                 Some(val) => val,
                 None => {
                     previous_head.next = rev_head;
                     return Some(head);
                 }
             };
-            for _ in 0..k {
-                let Some(new_tail) = &mut tail.next else {
-                    previous_head.next = rev_head;
-                    return Some(head);
-                };
-                tail = new_tail;
-            }
+
             let mut next_head = tail.next.take();
-            while let Some(mut new_head) = rev_head.take() {
-                rev_head = new_head.next.take();
-                // let new_head_next = new_head.next.take();
-                new_head.next = next_head;
+            while let Some(mut poped_node) = pop_and_advance_linked_node(&mut rev_head) {
+                poped_node.next = next_head;
+                next_head = Some(poped_node);
             }
+            previous_head.next = next_head;
+            previous_head = tail
         }
     }
 }
